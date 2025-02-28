b7bac327409605837a163c8db567273208ba882c
sen <sen@soracom.jp>
2025-02-28 02:59:25 +0000

    add deps

--- src/cargo-lambda/install.sh
+++ src/cargo-lambda/install.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 set -e
 
-if ! (which rustup > /dev/null && which cargo > /dev/null); then
-    which curl > /dev/null || (apt update && apt install curl -y -qq)
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    source $HOME/.cargo/env
-fi
-
-dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
-
-
-if ! cargo install --list | grep "cargo-binstall" > /dev/null; then
-    cargo install cargo-binstall
-fi
-
-umask 002
-if [ -z "${VERSION}" ]; then
+if [ -z "${VERSION}" ] || [ "${VERSION}" = "latest" ]; then
 	cargo binstall cargo-lambda --locked -y > /dev/null
 else
     cargo binstall cargo-lambda --locked -y --version $VERSION > /dev/null
 fi
-
-
